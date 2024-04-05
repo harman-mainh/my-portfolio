188d00d8930f3e4b92af8ce0f46ce2d07b09c4d5
harman-mainh <152292023+harman-mainh@users.noreply.github.com>
2024-04-05 09:46:15 -0400

    Add files via upload

---
+++ project/project1/function.js
@@ -0,0 +1,12 @@
+$("#switch").on('click',function(){
+    if($("body").hasClass("fire-off"))
+    {
+        $("body").removeClass("fire-off");
+        $("#switch").removeClass("switched");
+    }
+    else
+    {
+        $("body").addClass("fire-off");
+        $("#switch").addClass("switched");
+    }
+});
